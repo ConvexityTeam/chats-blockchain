c080f6bec6e1803514f69db8f6cb286242d02c28
justcharlz <charlie4biz@gmail.com>
2021-03-05 08:35:27 +0100

    Build Contract and deployed

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -2,5 +2,5 @@ const FreeMoney = artifacts.require('Chats.sol');
 
 module.exports = (deployer) => {
   deployer.deploy(FreeMoney, '1000000000000000000000000');
-  console.log('FreeMoney Token Contract is deployed');
+  console.log('CHATS Token Contract is deployed');
 };
