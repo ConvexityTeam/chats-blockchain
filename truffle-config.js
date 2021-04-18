49b49482088e6d6014484b190e1a2d367108a372
justcharlz <charlie4biz@gmail.com>
2021-04-18 01:29:27 +0100

    Edit truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -5,11 +5,11 @@ const BLOCKCHAINSERV = process.env.BLOCKCHAINSERV;
 
 module.exports = {
   networks: {
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
 
     // Used for the Azure network
     blockchainservice: {
