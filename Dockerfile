5b796383cf44612ddc2d9faa96df783f236311bb
Inim Andrew <inimandrew@gmail.com>
2021-07-04 18:15:46 +0100

    deployment

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:12.20.0-alpine
-WORKDIR /app
-COPY package*.json ./
-RUN npm i
-COPY . .
-EXPOSE 3000
-ENV NODE_ENV=production
-CMD [ "node", "/app/server.js" ]
--- migrations/2_initial_migration.js
+++
@@ -1,6 +0,0 @@
-const FreeMoney = artifacts.require('Chats.sol');
-
-module.exports = (deployer) => {
-  deployer.deploy(FreeMoney, '1000000000000000000000000');
-  console.log('CHATS Token Contract is deployed');
-};
