e90984a515e93501319788766879a1b52677f187
justcharlz <charlie4biz@gmail.com>
2021-10-12 13:52:46 +0100

    Fix: cor whitelisting

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,23 +1,5 @@
 const { Config } = require('./config')
 
 module.exports = {
-    Config,
-    // ApiResponse,
-    // GetLoggerInstance,
-    // HttpStatus,
-    // Errors,
-    // GetCodeMsg,
-    // CreateToken,
-    // GeneratePassword,
-    // DeSensitize,
-    // DeSensitizeUser,
-    // TrimObject,
-    // Random,
-    // GetXML,
-    // AddToCache, 
-    // GetFromCache, 
-    // AddOrUpdateUserCache,
-    // Encrypt,
-    // Decrypt,
-    // docUpload
+    Config
 }
