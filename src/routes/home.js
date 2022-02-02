2eedd5382aa0f39dfd07cd5bca1085f109de753a
justcharlz <charlie4biz@gmail.com>
2022-02-02 17:44:25 +0100

    Token service named

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const GetApp = async (req, res)=>{
     try {
-        return res.status(200).json({message: 'Welcome to CHATS token system'})
+        return res.status(200).json({message: 'Welcome to CHATS token service'})
     } catch (error) {
         res.status(500)
         return res.json({status: false, message: error})
