03432a277264cd0cc7ef8092d94367879d651898
justcharlz <charlie4biz@gmail.com>
2022-03-13 22:07:33 +0100

    fixes

--- src/routes/Transaction/index.js
+++ src/routes/Transaction/index.js
@@ -6,7 +6,7 @@ const { TransferAdmin, Transfers, Minting, Redeeming, Approve, TransferFrom, Des
 router.post('/transferadmin/:receiver/:amount', TransferAdmin);
 router.post('/transfer/:senderaddr/:senderpwsd/:receiver/:amount', Transfers);
 router.post('/mint/:amount/:mintTo', VerifyChecksum, Minting);
-router.post('/redeem/:senderaddr/:senderpswd/:amount', VerifyChecksum, Redeeming);
+router.post('/redeem/:senderaddr/:senderpswd/:amount', Redeeming);
 router.post('/distroyblackfund/:useraddr', DestroyBlackFunds);
 router.post('/approve/:tokenowneraddr/:tokenownerpswd/:spenderaddr/:amount', Approve);
 router.post('/transferfrom/:tokenowneraddr/:to/:spenderaddr/:spenderpwsd/:amount', TransferFrom);
